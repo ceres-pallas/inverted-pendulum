ef65fa24e2ae464360733f8a5d77bc02a64f21f4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-05-20 20:15:38 +0200

    create a currentState method

--- js/inverted-pendulum.js
+++ js/inverted-pendulum.js
@@ -1,3 +1,12 @@
 (function($){
-    $.InvertedPendulum = function(){};
+    var InvertedPendulum = $.InvertedPendulum = function(){};
+
+    InvertedPendulum.prototype.currentState = function(){
+	return {
+	    position : 0,
+	    angle: 0,
+	    velocity: 0,
+	    angularVelocity: 0
+	}
+    }
 })(window || module.exports);
--- spec/inverted-pendulum_spec.js
+++ spec/inverted-pendulum_spec.js
@@ -6,4 +6,17 @@ describe('Inverted Pendulum', function(){
     it('should be instantiated', function(){
 	expect(new InvertedPendulum()).toBeDefined();
     });
+
+    describe('states', function(){
+	it('should have current state', function(){
+	    var problem = new InvertedPendulum();
+	    var s = problem.currentState();
+
+	    expect(s.position).toBe(0);
+	    expect(s.angle).toBe(0);
+	    expect(s.velocity).toBe(0);
+	    expect(s.angularVelocity).toBe(0);
+	})
+    });
+
 });
