59a6d57a0ea7dd4beec1283c922e21a77eee5fc1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-01 13:01:10 +0200

    untabify source files

--- js/inverted-pendulum.js
+++ js/inverted-pendulum.js
@@ -1,37 +1,37 @@
 (function($){
     var copy = function(object) {
-	var c = {};
-	for (var key in object) {
-	    c[key] = object[key];
-	}
-	return c;
+        var c = {};
+        for (var key in object) {
+            c[key] = object[key];
+        }
+        return c;
     };
 
     var defaultState = {
-	position : 0,
-	angle: 0,
-	velocity: 0,
-	angularVelocity: 0,
-	delta : 1
+        position : 0,
+        angle: 0,
+        velocity: 0,
+        angularVelocity: 0,
+        delta : 1
     };
 
     var InvertedPendulum = $.InvertedPendulum = function(state){
-	this._currentState = copy(state || defaultState);
-	for (var key in defaultState) {
-	    if (!this._currentState[key]) {
-		this._currentState[key] = defaultState[key];
-	    }
-	}
+        this._currentState = copy(state || defaultState);
+        for (var key in defaultState) {
+            if (!this._currentState[key]) {
+                this._currentState[key] = defaultState[key];
+            }
+        }
     };
 
     InvertedPendulum.prototype.currentState = function(){
-	return copy(this._currentState);
+        return copy(this._currentState);
     }
     InvertedPendulum.prototype.tick = function(acceleration){
-	acceleration = acceleration || 0;
-	var state = this.currentState();
-	state.velocity += acceleration * state.delta;
-	state.position += state.velocity * state.delta;
-	this._currentState = state;
+        acceleration = acceleration || 0;
+        var state = this.currentState();
+        state.velocity += acceleration * state.delta;
+        state.position += state.velocity * state.delta;
+        this._currentState = state;
     }
 })(window || module.exports);
--- spec/inverted-pendulum_spec.js
+++ spec/inverted-pendulum_spec.js
@@ -1,155 +1,155 @@
 describe('Inverted Pendulum', function(){
     it('should exist', function(){
-	expect(InvertedPendulum).toBeDefined();
+        expect(InvertedPendulum).toBeDefined();
     });
 
     it('should be instantiated', function(){
-	expect(new InvertedPendulum()).toBeDefined();
+        expect(new InvertedPendulum()).toBeDefined();
     });
 
     describe('states', function(){
-	it('should have current state', function(){
-	    var problem = new InvertedPendulum();
-	    var s = problem.currentState();
+        it('should have current state', function(){
+            var problem = new InvertedPendulum();
+            var s = problem.currentState();
 
-	    expect(s.position).toBe(0);
-	    expect(s.angle).toBe(0);
-	    expect(s.velocity).toBe(0);
-	    expect(s.angularVelocity).toBe(0);
-	});
+            expect(s.position).toBe(0);
+            expect(s.angle).toBe(0);
+            expect(s.velocity).toBe(0);
+            expect(s.angularVelocity).toBe(0);
+        });
 
-	it('should be possible to override default state', function(){
-	    var start = {
-		position : 10,
-		angle: 5,
-		velocity: -3,
-		angularVelocity: 1
-	    };
-	    var problem = new InvertedPendulum(start);
-	    var s = problem.currentState();
+        it('should be possible to override default state', function(){
+            var start = {
+                position : 10,
+                angle: 5,
+                velocity: -3,
+                angularVelocity: 1
+            };
+            var problem = new InvertedPendulum(start);
+            var s = problem.currentState();
 
-	    expect(s.position).toBe(start.position);
-	    expect(s.angle).toBe(start.angle);
-	    expect(s.velocity).toBe(start.velocity);
-	    expect(s.angularVelocity).toBe(start.angularVelocity);
-	});
+            expect(s.position).toBe(start.position);
+            expect(s.angle).toBe(start.angle);
+            expect(s.velocity).toBe(start.velocity);
+            expect(s.angularVelocity).toBe(start.angularVelocity);
+        });
 
-	it('should complete state if not every detail is provided', function(){
-	    var start = { position: 1 };
-	    var problem = new InvertedPendulum(start);
-	    var s = problem.currentState();
+        it('should complete state if not every detail is provided', function(){
+            var start = { position: 1 };
+            var problem = new InvertedPendulum(start);
+            var s = problem.currentState();
 
-	    expect(s.position).toBe(start.position);
-	    expect(s.angle).toBe(0);
-	    expect(s.velocity).toBe(0);
-	    expect(s.angularVelocity).toBe(0);
-	});
+            expect(s.position).toBe(start.position);
+            expect(s.angle).toBe(0);
+            expect(s.velocity).toBe(0);
+            expect(s.angularVelocity).toBe(0);
+        });
     });
 
     describe('tick', function(){
-	it('should calculate stationary state correctly', function(){
-	    var problem = new InvertedPendulum();
+        it('should calculate stationary state correctly', function(){
+            var problem = new InvertedPendulum();
 
-	    problem.tick();
+            problem.tick();
 
-	    var s = problem.currentState();
-	    expect(s.position).toBe(0);
-	    expect(s.angle).toBe(0);
-	    expect(s.velocity).toBe(0);
-	    expect(s.angularVelocity).toBe(0);
-	});
+            var s = problem.currentState();
+            expect(s.position).toBe(0);
+            expect(s.angle).toBe(0);
+            expect(s.velocity).toBe(0);
+            expect(s.angularVelocity).toBe(0);
+        });
 
-	describe('with pendulum at PI/2', function(){
-	    var startAngle = Math.PI/2;
+        describe('with pendulum at PI/2', function(){
+            var startAngle = Math.PI/2;
 
-	    it('should calculate stationary state', function(){
-		var problem = new InvertedPendulum({
-		    position: 0, velocity: 0, angle: startAngle, angularVelocity: 0
-		});
+            it('should calculate stationary state', function(){
+                var problem = new InvertedPendulum({
+                    position: 0, velocity: 0, angle: startAngle, angularVelocity: 0
+                });
 
-		problem.tick();
+                problem.tick();
 
-		var s = problem.currentState();
-		expect(s.position).toBe(0);
-		expect(s.angle).toBe(startAngle);
-		expect(s.velocity).toBe(0);
-		expect(s.angularVelocity).toBe(0);
-	    });
+                var s = problem.currentState();
+                expect(s.position).toBe(0);
+                expect(s.angle).toBe(startAngle);
+                expect(s.velocity).toBe(0);
+                expect(s.angularVelocity).toBe(0);
+            });
 
-	    it('should calculate linear displacement', function(){
-		var problem = new InvertedPendulum({
-		    position: 0, velocity: 1, angle: startAngle, angularVelocity: 0
-		});
+            it('should calculate linear displacement', function(){
+                var problem = new InvertedPendulum({
+                    position: 0, velocity: 1, angle: startAngle, angularVelocity: 0
+                });
 
-		problem.tick();
+                problem.tick();
 
-		var s = problem.currentState();
-		expect(s.position).toBe(1);
-		expect(s.angle).toBe(startAngle);
-		expect(s.velocity).toBe(1);
-		expect(s.angularVelocity).toBe(0);
-	    });
+                var s = problem.currentState();
+                expect(s.position).toBe(1);
+                expect(s.angle).toBe(startAngle);
+                expect(s.velocity).toBe(1);
+                expect(s.angularVelocity).toBe(0);
+            });
 
-	    it('should constant acceleration correctly', function(){
-		var problem = new InvertedPendulum({
-		    position: 0, velocity: 0, angle: startAngle, angularVelocity: 0
-		});
+            it('should constant acceleration correctly', function(){
+                var problem = new InvertedPendulum({
+                    position: 0, velocity: 0, angle: startAngle, angularVelocity: 0
+                });
 
-		problem.tick(1);
+                problem.tick(1);
 
-		var s = problem.currentState();
-		expect(s.position).toBe(1);
-		expect(s.angle).toBe(startAngle);
-		expect(s.velocity).toBe(1);
-		expect(s.angularVelocity).toBe(0);
-	    });
-	});
+                var s = problem.currentState();
+                expect(s.position).toBe(1);
+                expect(s.angle).toBe(startAngle);
+                expect(s.velocity).toBe(1);
+                expect(s.angularVelocity).toBe(0);
+            });
+        });
 
-	describe('with altered delta', function(){
-	    var startAngle = Math.PI/2;
-	    var delta  = 1/2;
+        describe('with altered delta', function(){
+            var startAngle = Math.PI/2;
+            var delta  = 1/2;
 
-	    it('should calculate stationary state', function(){
-		var problem = new InvertedPendulum({
-		    angle: startAngle, delta: delta
-		});
+            it('should calculate stationary state', function(){
+                var problem = new InvertedPendulum({
+                    angle: startAngle, delta: delta
+                });
 
-		problem.tick();
+                problem.tick();
 
-		var s = problem.currentState();
-		expect(s.position).toBe(0);
-		expect(s.angle).toBe(startAngle);
-		expect(s.velocity).toBe(0);
-		expect(s.angularVelocity).toBe(0);
-	    });
+                var s = problem.currentState();
+                expect(s.position).toBe(0);
+                expect(s.angle).toBe(startAngle);
+                expect(s.velocity).toBe(0);
+                expect(s.angularVelocity).toBe(0);
+            });
 
-	    it('should calculate linear displacement', function(){
-		var problem = new InvertedPendulum({
-		    velocity: 1, angle: startAngle, delta: delta
-		});
+            it('should calculate linear displacement', function(){
+                var problem = new InvertedPendulum({
+                    velocity: 1, angle: startAngle, delta: delta
+                });
 
-		problem.tick();
+                problem.tick();
 
-		var s = problem.currentState();
-		expect(s.position).toBe(1/2);
-		expect(s.angle).toBe(startAngle);
-		expect(s.velocity).toBe(1);
-		expect(s.angularVelocity).toBe(0);
-	    });
+                var s = problem.currentState();
+                expect(s.position).toBe(1/2);
+                expect(s.angle).toBe(startAngle);
+                expect(s.velocity).toBe(1);
+                expect(s.angularVelocity).toBe(0);
+            });
 
-	    it('should constant acceleration correctly', function(){
-		var problem = new InvertedPendulum({
-		    angle: startAngle, delta: delta
-		});
+            it('should constant acceleration correctly', function(){
+                var problem = new InvertedPendulum({
+                    angle: startAngle, delta: delta
+                });
 
-		problem.tick(1);
+                problem.tick(1);
 
-		var s = problem.currentState();
-		expect(s.position).toBe(1/4);
-		expect(s.angle).toBe(startAngle);
-		expect(s.velocity).toBe(1/2);
-		expect(s.angularVelocity).toBe(0);
-	    });
-	});
+                var s = problem.currentState();
+                expect(s.position).toBe(1/4);
+                expect(s.angle).toBe(startAngle);
+                expect(s.velocity).toBe(1/2);
+                expect(s.angularVelocity).toBe(0);
+            });
+        });
     });
 });
