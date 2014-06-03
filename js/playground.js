6526a3c86bcc2997ce774d9b809c939b42f2579f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-03 21:32:51 +0200

    set initial angle off center

--- js/playground.js
+++ js/playground.js
@@ -1,7 +1,7 @@
 (function(World, View){
     var world = new World();
 
-    var problem = world.createInvertedPendulum();
+    var problem = world.createInvertedPendulum({ angle: Math.PI/50 });
 
     new View(document.getElementById('playground'), problem);
 })(World, InvertedPendulumView);
