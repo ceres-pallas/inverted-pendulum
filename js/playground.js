b72d52a173fb31680e747d33dc8ab62eeb4360ed
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-03 19:48:00 +0200

    include javascript source files

---
+++ js/playground.js
@@ -0,0 +1,7 @@
+(function(World, View){
+    var world = new World();
+
+    var problem = world.createInvertedPendulum();
+
+    new View(document.getElementById('playground', problem));
+})(World, InvertedPendulumView);
