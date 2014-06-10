012f4aa00e8aa505e5080fba1610eb8ee98e6fd8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-10 21:31:43 +0200

    use controller action

--- js/playground.js
+++ js/playground.js
@@ -1,13 +1,15 @@
-(function(World, View){
+(function(World, View, Controller){
     var world = new World({ g: 1/16, M: 1/4 });
 
     var problem = world.createInvertedPendulum({ angle: Math.PI/50 });
 
     new View(document.getElementById('playground'), problem);
 
+    var controller = new Controller();
+
     function run(){
-	problem.tick();
+	problem.tick(controller.action());
 	requestAnimationFrame(run);
     };
     run();
-})(World, InvertedPendulumView);
+})(World, InvertedPendulumView, Controller);
