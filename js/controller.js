473bcdaadff22285643437723614039fc4d6db13
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2014-06-29 12:33:28 +0200

    Made responses more realistic

--- js/controller.js
+++ js/controller.js
@@ -3,10 +3,10 @@
         this.direction = 0;
     };
     Controller.prototype.left = function(){
-        this.direction = -1;
+        this.direction = -0.1;
     };
     Controller.prototype.right = function(){
-        this.direction = 1;
+        this.direction = 0.1;
     };
     Controller.prototype.action = function(){
         var direction = this.direction;
