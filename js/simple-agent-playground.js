287637246c1a6fa5046ea2658bec0c84c20edc7d
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2014-06-29 12:32:50 +0200

    removed valuefunction

--- js/simple-agent-playground.js
+++ js/simple-agent-playground.js
@@ -1,48 +1,29 @@
 (function(World, View, FunctionApproximator, SimpleAgent){
-    var world = new World({ g: 1/16, M: 1/4 });
+    var world = new World({ g: 1/160, M: 1/4 });
 
-    var initialState = { angle: Math.PI/50 };
+    var initialState = { angle: Math.PI/50, ended: false };
     var problem = world.createInvertedPendulum(initialState);
 
     new View(document.getElementById('playground'), problem);
 
     var functionApproximator = new FunctionApproximator(
 	function(){return Math.random();},
+	0.001, 
 	0.01, 
-	0.1, 
 	function(elements) { return elements[Math.floor((Math.random() * elements.length))]; }
     );
 
     functionApproximator.addValueFunction(
 	functionApproximator.createValueFunction(function(s) { 
-	    return s.angle; 
+	    return 1;
 	})
-    );
-
-    functionApproximator.addValueFunction(
-	functionApproximator.createValueFunction(function(s) { 
-	    return s.angularVelocity; 
-	})
-    );
-
-    functionApproximator.addValueFunction(
-	functionApproximator.createValueFunction(function(s) { 
-	    return s.velocity; 
-	})
-    );
-
-    functionApproximator.addValueFunction(
-	functionApproximator.createValueFunction(function(s) { 
-	    return s.position; 
-	})
-    );
+    ); 
 
     var agent = new SimpleAgent(problem, functionApproximator);
     var count = 0;
     function run(){
-	count++;
 	if(!problem.currentState().ended) {
-	    if(count%1 == 0) {
+	    if(count%5 == 0) {
 		var option = agent.chooseAction();
 	    
 		agent.performAction(option.action);
@@ -56,7 +37,9 @@
 
 	    var log = "";
 	    agent.solver.getValueFunctions().forEach(function(el) { log += " " +  el.getWeight(); } );
-	    console.log(log);
+	    //log weights
+	    //console.log(log);
+	
 	    problem.currentState(initialState);
 
 	}
