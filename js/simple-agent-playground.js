31cbdb046558f6b91f18e0821b65678d42ba5d66
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-03-22 19:17:36 +0100

    Satisfy linter

--- js/simple-agent-playground.js
+++ js/simple-agent-playground.js
@@ -7,46 +7,41 @@
     new View(document.getElementById('playground'), problem);
 
     var functionApproximator = new FunctionApproximator(
-	function(){return Math.random();},
-	0.001,
-	0.01,
-	function(elements) { return elements[Math.floor((Math.random() * elements.length))]; },
-	function(weights) {
-	    return 1/weights.reduce(function (prev, current) { return Math.max(prev, current)}, -Infinity)
-	}
+	      function(){return Math.random();},
+	      0.001,
+	      0.01,
+	      function(elements) { return elements[Math.floor((Math.random() * elements.length))]; },
+	      function(weights) {
+	          return 1/weights.reduce(function (prev, current) { return Math.max(prev, current);}, -Infinity);
+	      }
     );
 
     functionApproximator.addValueFunction(
-	functionApproximator.createValueFunction(function(s) {
-	    return 1;
-	})
+	      functionApproximator.createValueFunction(function(s) {
+	          return 1;
+	      })
     );
 
     var agent = new SimpleAgent(problem, functionApproximator);
     var count = 0;
     function run(){
-	if(!problem.currentState().ended) {
-	    if(count%5 == 0) {
-		var option = agent.chooseAction();
-
-		agent.performAction(option.action);
-	    } else {
-		problem.tick();
-	    }
-	    count++;
-	} else {
-
-	    agent.reevaluateActions();
-
-	    var log = "";
-	    agent.solver.getValueFunctions().forEach(function(el) { log += " " +  el.getWeight(); } );
-	    //log weights
-	    //console.log(log);
-
-	    problem.currentState(initialState);
-
-	}
-	requestAnimationFrame(run);
+	      if(!problem.currentState().ended) {
+	          if(count%5 == 0) {
+		            var option = agent.chooseAction();
+		            agent.performAction(option.action);
+	          } else {
+		            problem.tick();
+	          }
+	          count++;
+	      } else {
+	          agent.reevaluateActions();
+	          var log = "";
+	          agent.solver.getValueFunctions().forEach(function(el) { log += " " +  el.getWeight(); } );
+	          //log weights
+	          //console.log(log);
+	          problem.currentState(initialState);
+	      }
+	      requestAnimationFrame(run);
     };
     run();
 })(World, InvertedPendulumView, FunctionApproximator, SimpleAgent);
